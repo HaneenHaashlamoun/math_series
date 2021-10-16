ee4841f668da270d73a783c5f2e0fcfea7ed2cae
Haneen Hashlamoun <haneen.hashlamoun@gmail.com>
2021-10-16 13:29:40 +0300

    fix lab2

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,5 @@
 def fibonacci(n):
+    #Base Case
     if n == 0:
         return 0
     elif n == 1:
@@ -6,4 +7,24 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
+def lucas(n):
+    #Base Case
+    if n <= 1:
+        if n == 0:
+            return 2
+        else:
+            return 1
+    return lucas(n-1) + lucas(n-2)
 
+
+def sum_series(n, first=0, second=1):
+    if n == 0:
+        #Base Case
+        return first
+    elif n == 1:
+        return second
+    else:
+        return sum_series(n - 1, first, second) + sum_series(n - 2, first, second)
+print(sum_series(3, 1, 2))
+print(fibonacci(5))
+print(lucas(5))
